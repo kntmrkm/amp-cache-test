fc5ce66cd08f0a90de80eb5c75cad84f30fbaccb
kntmrkm <kntmrkm@gmail.com>
2019-04-09 17:29:01 +0900

    layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def home
+    if request.format.symbol == :amp
+    end
+
+    respond_to do |format|
+      format.html
+      format.amp do
+        lookup_context.formats = [:amp, :html] # .htmlのテンプレートも検索する
+        render layout: 'amp'
+      end
+    end
   end
 end
