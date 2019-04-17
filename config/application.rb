785307d12a88e94e0e6d4c53ef9a7a0fc5be4dc4
kntmrkm <kntmrkm@gmail.com>
2019-04-17 10:17:39 +0900

    MIME_TYPES settings

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module AmpCacheTest
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # curl -I http:/lvh.me:3002/.well-known/amphtml/apikey.pub
+    # ルーティングを作って返してもよいはず
+    Rack::Mime::MIME_TYPES[".pub"]="text/plain"
   end
 end
