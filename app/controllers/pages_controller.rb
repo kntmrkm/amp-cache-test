f4bf7cb2358d36d082e0329f3efb7d962c6da5a9
kntmrkm <kntmrkm@gmail.com>
2019-04-17 16:45:33 +0900

    Remove max-age

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,7 @@ class PagesController < ApplicationController
   end
 
   def home
-    expires_in 1.minute #, public: true
-
-    if request.format.symbol == :amp
-    end
+    #expires_in 1.minute #, public: true
 
     respond_to do |format|
       format.html
